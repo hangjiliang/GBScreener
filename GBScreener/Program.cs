824ebe4fdcae8dccc152b376355ee5ac05182267
Jiliang Hang <jhang@assurity.com>
2016-06-15 14:25:07 -0500

    initial

---
+++ GBScreener/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ServiceModel.Syndication;
+using System.Xml;
+using System;
+
+namespace GBScreener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://rssfeeds.s3.amazonaws.com/goldbox";
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+            foreach (SyndicationItem item in feed.Items)
+            {
+                string subject = item.Title.Text;
+                //string summary = item.Summary.Text;
+                //Console.WriteLine(subject);
+                var links = item.Links;
+                var itemUri = links[0].Uri.AbsolutePath;
+                Console.WriteLine(itemUri);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ GBScreener/Screener/ScreenUtil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBScreener.Screener
+{
+    public class ScreenUtil
+    {
+        public const string reviewUriPrefix = "http://www.amazon.com/product-reviews/";
+
+        public int getReviewNumber(string asin)
+        {
+            int reviewNo = -1;
+            string reviewUri = reviewUriPrefix + asin;
+
+            return reviewNo;
+        }
+    }
+}
